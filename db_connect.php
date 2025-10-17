a29d54633c223c9948dc186dc5aeb08924f65e0a
Ola-xooox <lhandelpamisa9@gmail.com>
2025-10-17 18:18:04 +0800

    ...

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root"; // Default XAMPP username
-$password = "";     // Default XAMPP password
-$dbname = "pharmacy_system";
+$servername = "sql209.infinityfree.com";
+$username = "if0_40188284"; // Default XAMPP username
+$password = "mjpharmacy11770";     // Default XAMPP password
+$dbname = "if0_40188284_pharmacy_system";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
